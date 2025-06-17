5314f524eac0b1f5fe46506e407dd0926530ca2c
s19835 <s19835@sci.pdn.ac.lk>
2025-06-17 22:14:12 +0530

    add: crypto standard implementation

--- benchmarks/crypto_standard.go
+++ benchmarks/crypto_standard.go
@@ -1,1 +1,19 @@
 package benchmarks
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+var key = []byte("thisis32bitlongpassphraseimusing") // 32 bytes AES-256
+var iv = []byte("thisis16bytesiv!")                  // 16 byte IV
+
+func EncryptStandard(plaintext []byte) []byte {
+	block, _ := aes.NewCipher(key)
+	stream := cipher.NewCTR(block, iv)
+
+	ciphertext := make([]byte, len(plaintext))
+	stream.XORKeyStream(ciphertext, plaintext)
+
+	return ciphertext
+}
