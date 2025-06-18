9fa7a21986b2da17356a8c4fe151b1b5a78fecb9
s19835 <s19835@sci.pdn.ac.lk>
2025-06-18 17:58:38 +0530

    add: main file

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/s19835/aes-benchmark-go
 go 1.24.1
 
 require (
-	github.com/gtank/cryptopasta v0.0.0-20170601214702-1f550f6f2f69 // indirect
+	github.com/gtank/cryptopasta v0.0.0-20170601214702-1f550f6f2f69
 	golang.org/x/crypto v0.39.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/s19835/aes-benchmark-go/benchmarks"
+)
 
 func main() {
-	fmt.Println("Everthings look good!")
+	inputsize := []string{"1KB", "1MB", "100MB"}
+	methods := []string{"standard", "cryptopasta"}
+
+	for _, method := range methods {
+		for _, size := range inputsize {
+			data, err := os.ReadFile("benchmarks/data/" + size + ".txt")
+			if err != nil {
+				panic(err)
+			}
+
+			start := time.Now()
+			var enc []byte
+
+			switch method {
+			case "standard":
+				enc = benchmarks.EncryptStandard(data)
+			case "cryptopasta":
+				enc = benchmarks.EncryptCryptopasta(data)
+			}
+
+			elapsed := time.Since(start)
+			fmt.Printf("Method: %s | Size: %s | Time: %s | Encrypted Length: %d\n", method, size, elapsed, len(enc))
+		}
+	}
 }
